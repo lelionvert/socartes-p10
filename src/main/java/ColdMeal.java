83d685ad7b55e628657d18173a1da087181c1c30
yohan <damien.lequeux@gmail.com>
2019-08-19 14:29:39 +0200

    Refacto isSameDates and test method

--- src/main/java/ColdMeal.java
+++ src/main/java/ColdMeal.java
@@ -1,8 +1,10 @@
+import java.time.LocalDate;
 import java.util.Date;
+
 
 public class ColdMeal {
 
-    public static int compareDates(Date arrivalDate, Date referenceDate){
-        return 1;
+    public static boolean isSameDates(LocalDate arrivalDate, LocalDate referenceDate){
+        return true;
     }
 }
--- src/test/java/ColdMealTest.java
+++ src/test/java/ColdMealTest.java
@@ -2,18 +2,17 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class ColdMealTest {
 
     @Test
-    public void when_arrival_date_is_the_same_as_the_reference_date_then_should_return_one() throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy");
-        Date arrivalDate = sdf.parse("17-10-2019");
-        Date referenceDate = sdf.parse("17-10-2019");
+    public void when_arrival_date_is_the_same_as_the_conference_date_then_should_return_one() throws ParseException {
+        LocalDate arrivalDate = LocalDate.of(2019,10,17);
+        LocalDate conferenceDate = LocalDate.of(2019,10,17);
 
-        assertEquals(1, ColdMeal.compareDates(arrivalDate, referenceDate));
+        assertTrue(ColdMeal.isSameDates(arrivalDate, conferenceDate));
     }
 }
