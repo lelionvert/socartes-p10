6a9d302716f01fbcb0e538a99e5c2cd2c0576a96
sandymane <manesandy44@gmail.com>
2019-08-19 15:27:55 +0200

    Start to implement count coldMeals

--- src/test/java/ColdMealTest.java
+++ src/test/java/ColdMealTest.java
@@ -5,15 +5,25 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.*;
+
 public class ColdMealTest {
 
     @Test
     public void count_cold_meals_should_return_1_when_date_is_after_nine() {
         int numberOfColdMeals = 1;
-        Assert.assertEquals(numberOfColdMeals, countColdMeals(Arrays.asList(LocalDateTime.of(2019,10,17,22,00))));
+        assertEquals(numberOfColdMeals, countColdMeals(Arrays.asList(LocalDateTime.of(2019,10,17,22,00))));
+    }
+
+    @Test
+    public void count_cold_meals_should_return_0_when_date_is_before_nine() {
+        int numberOfColdMeals = 0;
+        assertEquals(numberOfColdMeals, countColdMeals(Arrays.asList(LocalDateTime.of(2019, 10, 17, 20, 00))));
     }
 
     private int countColdMeals(List<LocalDateTime> asList) {
-        return 1;
+        if(((LocalDateTime)asList.get(0)).isAfter(LocalDateTime.of(2019,10,17,21,00)))
+            return 1;
+        return 0;
     }
 }
