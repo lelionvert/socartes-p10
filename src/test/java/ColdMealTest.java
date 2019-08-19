3bafd914fb58b217c868d98c8b3c91278ea7cb22
sandymane <manesandy44@gmail.com>
2019-08-19 15:30:56 +0200

    refacto test and method

--- src/test/java/ColdMealTest.java
+++ src/test/java/ColdMealTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
@@ -9,20 +8,28 @@ import static org.junit.Assert.*;
 
 public class ColdMealTest {
 
+    public static final LocalDateTime COLD_MEALS_START_TIME = LocalDateTime.of(2019, 10, 17, 21, 00);
+
     @Test
     public void count_cold_meals_should_return_1_when_date_is_after_nine() {
         int numberOfColdMeals = 1;
-        assertEquals(numberOfColdMeals, countColdMeals(Arrays.asList(LocalDateTime.of(2019,10,17,22,00))));
+
+        int result = countColdMeals(Arrays.asList(LocalDateTime.of(2019, 10, 17, 22, 00)));
+        
+        assertEquals(numberOfColdMeals, result);
     }
 
     @Test
     public void count_cold_meals_should_return_0_when_date_is_before_nine() {
         int numberOfColdMeals = 0;
-        assertEquals(numberOfColdMeals, countColdMeals(Arrays.asList(LocalDateTime.of(2019, 10, 17, 20, 00))));
+        
+        int result = countColdMeals(Arrays.asList(LocalDateTime.of(2019, 10, 17, 20, 00)));
+        
+        assertEquals(numberOfColdMeals, result);
     }
 
     private int countColdMeals(List<LocalDateTime> asList) {
-        if(((LocalDateTime)asList.get(0)).isAfter(LocalDateTime.of(2019,10,17,21,00)))
+        if(((LocalDateTime)asList.get(0)).isAfter(COLD_MEALS_START_TIME))
             return 1;
         return 0;
     }
