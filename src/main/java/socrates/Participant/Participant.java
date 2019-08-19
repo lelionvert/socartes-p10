535b1f4532cd08b32d2c687977c9da0d5b63c116
sandymane <manesandy44@gmail.com>
2019-08-19 10:36:29 +0200

    check if participant arrive before 9PM

--- src/main/java/socrates/Participant/Participant.java
+++ src/main/java/socrates/Participant/Participant.java
@@ -7,4 +7,12 @@ public class Participant {
     public Date getDate() {
         return new Date();
     }
+
+    public Date setDate(Date date) {
+        return null;
+    }
+
+    public boolean comeAfterNinePM() {
+        return true;
+    }
 }
--- src/test/java/ParticipantTest.java
+++ src/test/java/ParticipantTest.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import socrates.Participant.Participant;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class ParticipantTest {
@@ -9,5 +11,12 @@ public class ParticipantTest {
     @Test
     public void participant_should_have_checkin_date() {
         assertNotNull(new Participant().getDate());
+    }
+
+    @Test
+    public void check_participant_arrival_after_nine_should_return_true() {
+        Participant participant = new Participant();
+        participant.setDate(new Date(2019, 10, 10, 22, 00));
+        assertTrue(participant.comeAfterNinePM());
     }
 }
