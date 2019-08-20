e96c0b5d7fb674a0bed55d3f2b75a8da20fdcc32
haifa2017 <eljarroudihaifa@gmail.com>
2019-08-20 12:02:04 +0200

    Refacto

--- promo-10/ColdMeal/ColdMeal.cs
+++ promo-10/ColdMeal/ColdMeal.cs
@@ -6,10 +6,12 @@ namespace ColdMeals
 {
     public class ColdMeal
     {
-        public int Compute(in IList<DateTime> checkins)
+        private const int COLD_MEAL_START_TIME = 21;
+
+        public int Count(in IList<DateTime> checkins)
         {
             DateTime checkin = checkins.FirstOrDefault();
-            if (checkin.Hour < 21)
+            if (checkin.Hour < COLD_MEAL_START_TIME)
                 return 0;
             return checkins.Count;
         }
--- promo-10/ColdMealTest/ColdMealTest.cs
+++ promo-10/ColdMealTest/ColdMealTest.cs
@@ -18,44 +18,49 @@ namespace Tests
         }
         
         [Test]
-        public void cold_meal_number_should_be_zero_given_no_checkin()
+        public void cold_meal_amount_should_be_zero_given_no_checkin()
         {
             List<DateTime> checkins = new List<DateTime>();
-            int coldMealNumber = coldMeal.Compute(checkins);
+            int coldMealNumber = coldMeal.Count(checkins);
             coldMealNumber.Should().Be(0);
         }
         
         [Test]
-        public void cold_meal_number_should_be_one_given_a_late_checkin()
+        public void cold_meal_amount_should_be_one_given_a_late_checkin()
         {
             IList<DateTime> checkIns = new List<DateTime>
             {
-                new DateTime(2019,10,19,22,00,00)
+                ConferenceStartDayCheckInOf(22)
             };
-            int coldMealNumber = coldMeal.Compute(checkIns);
+            int coldMealNumber = coldMeal.Count(checkIns);
             coldMealNumber.Should().Be(1);
         }
 
         [Test]
-        public void cold_meal_number_should_be_zero_given_an_early_check_in()
+        public void cold_meal_amount_should_be_zero_given_an_early_check_in()
         {
             IList<DateTime> checkIns = new List<DateTime>
             {
-                new DateTime(2019, 10, 19, 17,0, 0)
+                ConferenceStartDayCheckInOf(17)
             };
-            int coldMealNumber = coldMeal.Compute(checkIns);
+            int coldMealNumber = coldMeal.Count(checkIns);
             coldMealNumber.Should().Be(0);
         }
 
         [Test]
-        public void cold_meal_number_should_be_zero_given_an_early_check_in_triangulation()
+        public void cold_meal_amount_should_be_zero_given_an_early_check_in_triangulation()
         {
             IList<DateTime> checkins = new List<DateTime>
             {
-                new DateTime(2019, 10, 19, 20,0,0 )
+                ConferenceStartDayCheckInOf(20)
             };
-            int coldMealNumber = coldMeal.Compute(checkins);
+            int coldMealNumber = coldMeal.Count(checkins);
             coldMealNumber.Should().Be(0);
+        }
+
+        private static DateTime ConferenceStartDayCheckInOf(int hour)
+        {
+            return new DateTime(2019, 10, 19, hour,0,0);
         }
     }
 }
