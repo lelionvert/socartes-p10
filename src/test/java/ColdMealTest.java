771b8f8d7dab900b2f5972e1dbdc6b49534d8e1b
yohan <damien.lequeux@gmail.com>
2019-08-20 15:46:47 +0200

    Refactor test methods

--- src/test/java/ColdMealTest.java
+++ src/test/java/ColdMealTest.java
@@ -11,11 +11,8 @@ public class ColdMealTest {
 
     @Test
     public void when_arrival_date_is_same_as_conference_date_and_after_nine_PM_then_should_return_true() {
-        ColdMeal coldMeal = getColdMeal();
-
-        LocalDate arrivalDay = LocalDate.of(2019, 10, 17);
-        LocalTime arrivalHour = LocalTime.of(22, 0);
-        CheckinDate arrivalDate = new CheckinDate(arrivalDay, arrivalHour);
+        ColdMeal coldMeal = setUpColdMeal();
+        CheckinDate arrivalDate = setUpCheckin(2019,10,17, 22, 0);
 
         assertTrue(coldMeal.needOneColdMeal(arrivalDate));
 
@@ -23,21 +20,16 @@ public class ColdMealTest {
 
     @Test
     public void when_arrival_date_and_hour_is_same_as_cold_meal_start_time_return_false() {
-        ColdMeal coldMeal = getColdMeal();
+        ColdMeal coldMeal = setUpColdMeal();
+        CheckinDate arrivalDate = setUpCheckin(2019,10,17, 21, 0);
 
-        LocalDate arrivalDay = LocalDate.of(2019, 10, 17);
-        LocalTime arrivalHour = LocalTime.of(21, 0);
-        CheckinDate arrivalDate = new CheckinDate(arrivalDay, arrivalHour);
         assertFalse(coldMeal.needOneColdMeal(arrivalDate));
     }
 
     @Test
     public void when_arrival_date_and_hour_is_same_as_cold_meal_end_time_return_false() {
-        ColdMeal coldMeal = getColdMeal();
-
-        LocalDate arrivalDay = LocalDate.of(2019, 10, 18);
-        LocalTime arrivalHour = LocalTime.of(0, 0);
-        CheckinDate arrivalDate = new CheckinDate(arrivalDay, arrivalHour);
+        ColdMeal coldMeal = setUpColdMeal();
+        CheckinDate arrivalDate = setUpCheckin(2019,10,18, 0, 0);
 
         assertFalse(coldMeal.needOneColdMeal(arrivalDate));
     }
@@ -45,8 +37,7 @@ public class ColdMealTest {
     @Test
     public void when_no_checking_then_dont_count_any_cold_meal() {
         int expectedQuantityOfColdMeals = 0;
-
-        ColdMeal coldMeal = getColdMeal();
+        ColdMeal coldMeal = setUpColdMeal();
 
         assertEquals(expectedQuantityOfColdMeals, coldMeal.count(null));
     }
@@ -54,17 +45,22 @@ public class ColdMealTest {
     @Test
     public void when_one_checkin_in_cold_meal_range_then_count_one_cold_meal() {
         int expectedQuantityOfColdMeal = 1;
-
-        ColdMeal coldMeal = getColdMeal();
+        ColdMeal coldMeal = setUpColdMeal();
         CheckinDate checkinDate = new CheckinDate(LocalDate.of(2019, 10, 17),LocalTime.of(22,0));
 
         assertEquals(expectedQuantityOfColdMeal, coldMeal.count(checkinDate));
     }
 
-    private ColdMeal getColdMeal() {
+    private ColdMeal setUpColdMeal() {
         LocalTime beginHourForColdMeal = LocalTime.of(21, 0);
         LocalDate dayOfColdMeal = LocalDate.of(2019, 10, 17);
         CheckinDate timeLimitForColdMeal = new CheckinDate(dayOfColdMeal, beginHourForColdMeal);
         return new ColdMeal(timeLimitForColdMeal);
+    }
+
+    private CheckinDate setUpCheckin(int year, int month, int day, int hour, int minute) {
+        LocalDate arrivalDay = LocalDate.of(year, month, day);
+        LocalTime arrivalHour = LocalTime.of(hour, minute);
+        return new CheckinDate(arrivalDay, arrivalHour);
     }
 }
