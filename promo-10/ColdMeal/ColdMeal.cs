7cc7aebcfcc0be96d0470a268729c78c05a54636
haifa2017 <eljarroudihaifa@gmail.com>
2019-08-20 09:45:49 +0200

    Test no cold meal given when early check-in and Refacto

--- promo-10/ColdMeal/ColdMeal.cs
+++ promo-10/ColdMeal/ColdMeal.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ColdMeals
 {
     public class ColdMeal
     {
-        public int Compute(in List<DateTime> checkins)
+        public int Compute(in IList<DateTime> checkins)
         {
+            DateTime checkin = checkins.FirstOrDefault();
+            if (checkin.Hour == 17)
+                return 0;
             return checkins.Count;
         }
     }
--- promo-10/ColdMealTest/ColdMealTest.cs
+++ promo-10/ColdMealTest/ColdMealTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using ColdMeals;
 using FluentAssertions;
 using NUnit.Framework;
@@ -27,12 +28,23 @@ namespace Tests
         [Test]
         public void cold_meal_number_should_be_one_given_a_late_checkin()
         {
-            List<DateTime> checkIns = new List<DateTime>
+            IList<DateTime> checkIns = new List<DateTime>
             {
                 new DateTime(2019,10,19,22,00,00)
             };
             int coldMealNumber = coldMeal.Compute(checkIns);
             coldMealNumber.Should().Be(1);
+        }
+
+        [Test]
+        public void cold_meal_number_should_be_zero_given_an_early_check_in()
+        {
+            IList<DateTime> checkIns = new List<DateTime>
+            {
+                new DateTime(2019, 10, 19, 17,0, 0)
+            };
+            int coldMealNumber = coldMeal.Compute(checkIns);
+            coldMealNumber.Should().Be(0);
         }
     }
 }
