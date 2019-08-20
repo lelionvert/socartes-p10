a4c842e6919c98a7a6791562ad049978abeef4fa
sandymane <manesandy44@gmail.com>
2019-08-20 09:32:16 +0200

    Rename test method

--- src/test/java/socrates/ColdMealTest.java
+++ src/test/java/socrates/ColdMealTest.java
@@ -14,62 +14,61 @@ public class ColdMealTest {
     private final ColdMeals coldMeals = new ColdMeals();
 
     @Test
-    public void count_cold_meals_should_return_1_when_date_is_after_nine() {
-        int numberOfColdMeals = 1;
+    public void count_cold_meals_should_return_1_when_date_is_after_nine_and_before_midnight() {
+        int expectedNumberOfColdMeals = 1;
 
         int result = coldMeals.countColdMeals(Collections.singletonList(aDate(17, 22, 0)));
 
-        assertEquals(numberOfColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     @Test
     public void count_cold_meals_should_return_0_when_date_is_before_nine() {
-        int numberOfColdMeals = 0;
+        int expectedNumberOfColdMeals = 0;
 
         int result = coldMeals.countColdMeals(Collections.singletonList(aDate(17, 20, 0)));
 
-        assertEquals(numberOfColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     @Test
     public void count_cold_meals_should_return_0_when_date_is_empty() {
-        int numberColdMeals = 0;
+        int expectedNumberOfColdMeals = 0;
 
         int result = coldMeals.countColdMeals(Collections.emptyList());
 
-        assertEquals(numberColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     @Test
-    public void count_cold_meals_should_return_2_when_list_of_date() {
-        int numberOfColdMeals = 2;
+    public void count_cold_meals_should_return_2_when_list_of_date_contains_2_date_between_nine_and_midnight() {
+        int expectedNumberOfColdMeals = 2;
 
         int result = coldMeals.countColdMeals(Arrays.asList(aDate(17, 22, 0),
                 aDate(17, 22, 30)));
 
-        assertEquals(numberOfColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     @Test
-    public void count_cold_meals_should_return_0_when_list_of_date() {
-        int numberOfColdMeals = 0;
+    public void count_cold_meals_should_return_0_when_only_limit_date() {
+        int expectedNumberOfColdMeals = 0;
 
         int result = coldMeals.countColdMeals(Collections.singletonList(aDate(17, 21, 0)));
 
-        assertEquals(numberOfColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     @Test
     public void count_cold_meals_should_return_0_when_date_after_midnight() {
-        int numberOfColdMeals = 0;
+        int expectedNumberOfColdMeals = 0;
 
         int result = coldMeals.countColdMeals(Collections.singletonList(aDate(18, 0, 0)));
 
-        assertEquals(numberOfColdMeals, result);
+        assertEquals(expectedNumberOfColdMeals, result);
     }
 
     private static LocalDateTime aDate(int day, int hour, int minute) {
         return LocalDateTime.of(2019, 10, day, hour, minute);
     }
-
 }
