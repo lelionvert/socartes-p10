39cea615655672658dad99dbd0c8866c06fdae77
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-08-21 11:39:32 +0200

    Refactored test method

--- src/test/java/PriceCalculatorTest.java
+++ src/test/java/PriceCalculatorTest.java
@@ -1,12 +1,12 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class PriceCalculatorTest {
     @Test
-    public void if_full_single_then_price_of_single() {
+    public void when_book_a_single_room_then_return_a_single_room_price() {
         int calculatedPrice = PriceCalculator.compute();
-        assertEquals(610, calculatedPrice);
+        int expectedPrice = 610;
+        assertEquals(expectedPrice, calculatedPrice);
     }
 }
