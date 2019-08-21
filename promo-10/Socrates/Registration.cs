43687741addfff4cf5ef455fddd94d56693dfc35
Yohan F <yohan.fairfort@lacombedulionvert.fr>
2019-08-21 15:43:56 +0200

    Test room type triple

--- promo-10/Socrates/Registration.cs
+++ promo-10/Socrates/Registration.cs
@@ -6,13 +6,15 @@ namespace Socrates
     {
         private const int ROOM_PRICE_DOUBLE = 510;
         private const int ROOM_PRICE_SINGLE = 610;
+        private const int ROOM_PRICE_TRIPLE = 410;
 
         public int Price(RoomType roomType)
         {
             if (RoomType.Double == roomType)
-            {
                 return ROOM_PRICE_DOUBLE;
-            }
+
+            if (RoomType.Triple == roomType)
+                return ROOM_PRICE_TRIPLE;
 
             return ROOM_PRICE_SINGLE;
         }
