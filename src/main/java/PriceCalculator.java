61aaa92042a844a220c7f1471dea15cb684c495d
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-08-21 16:13:44 +0200

    Add RoomType Enum

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -2,14 +2,17 @@ public class PriceCalculator {
 
     private static final int SINGLE_ROOM_FULL_PRICE = 610;
     private static final int DOUBLE_ROOM_FULL_PRICE = 510;
+    private static final int TRIPLE_ROOM_FULL_PRICE = 410;
+    private static final int NO_ACCOMMODATION_FULL_PRICE = 240;
 
-    public static int compute(String roomType) {
-        if("double".equals(roomType))
+    public static int computePrice(RoomType roomType) {
+        if(RoomType.DOUBLE.equals(roomType))
             return DOUBLE_ROOM_FULL_PRICE;
-        if("triple".equals(roomType))
-            return 410;
-        if("no accommodation".equals(roomType))
-            return 240;
+        if(RoomType.TRIPLE.equals(roomType))
+            return TRIPLE_ROOM_FULL_PRICE;
+        if(RoomType.NO_ACCOMMODATION.equals(roomType))
+            return NO_ACCOMMODATION_FULL_PRICE;
         return SINGLE_ROOM_FULL_PRICE;
     }
+
 }
--- src/test/java/PriceCalculatorTest.java
+++ src/test/java/PriceCalculatorTest.java
@@ -7,10 +7,10 @@ public class PriceCalculatorTest {
     @Test
     public void acceptance_test_complete_price_scenario() {
         int[] expected = {610, 510, 410, 240};
-        String[] input = {"single", "double", "triple", "no accommodation"};
+        RoomType[] input = {RoomType.SINGLE, RoomType.DOUBLE, RoomType.TRIPLE, RoomType.NO_ACCOMMODATION};
         for (int i = 0; i < expected.length; i++) {
-            String roomType = input[i];
-            int calculatedPrice = PriceCalculator.compute(roomType);
+            RoomType roomType = input[i];
+            int calculatedPrice = PriceCalculator.computePrice(roomType);
             int expectedPrice = expected[i];
             assertEquals(expectedPrice, calculatedPrice);
         }
@@ -18,14 +18,14 @@ public class PriceCalculatorTest {
 
     @Test
     public void when_book_a_single_room_with_full_stay_then_return_a_single_room_full_price() {
-        int calculatedPrice = PriceCalculator.compute("single");
+        int calculatedPrice = PriceCalculator.computePrice(RoomType.SINGLE);
         int singleRoomFullPrice = 610;
         assertEquals(singleRoomFullPrice, calculatedPrice);
     }
 
     @Test
     public void when_book_double_room_with_full_stay_then_return_double_room_full_price() {
-        int calculatedPrice = PriceCalculator.compute("double");
+        int calculatedPrice = PriceCalculator.computePrice(RoomType.DOUBLE);
         int doubleRoomFullPrice = 510;
         assertEquals(doubleRoomFullPrice, calculatedPrice);
     }
