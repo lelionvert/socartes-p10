642e6dd43f32ddb5b11652b4f2bcfe244757ab29
Yohan F <yohan.fairfort@lacombedulionvert.fr>
2019-08-21 14:41:39 +0200

    Rename magic number and test method

--- promo-10/SocratesTest/RegistrationTest.cs
+++ promo-10/SocratesTest/RegistrationTest.cs
@@ -13,10 +13,11 @@ namespace SocratesTest
         }
 
         [Test]
-        public void given_a_checkin_on_thursday_and_checkout_on_saturday_dinner_with_single_choice_the_price_should_be_610()
+        public void given_a_checkin_on_thursday_and_checkout_on_sunday_lunch_with_single_choice_the_price_should_be_single_complete_price()
         {
             int totalAmount = Registration.Price();
-            totalAmount.Should().Be(610);
+            int singleCompletePrice = 610;
+            totalAmount.Should().Be(singleCompletePrice);
         }
     }
 }
