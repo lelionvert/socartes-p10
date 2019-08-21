36a20fbdf741f883c0676d7b72c9409d112d007b
yohan <damien.lequeux@gmail.com>
2019-08-21 11:32:15 +0200

    implements calculate price test for double

--- src/test/java/CalculatePriceTest.java
+++ src/test/java/CalculatePriceTest.java
@@ -1,9 +1,10 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatePriceTest {
 
@@ -16,10 +17,25 @@ public class CalculatePriceTest {
 
         int resultPrice = calculatePrice(checkinDate, checkoutDate, accommodation);
 
-        Assert.assertEquals(expectedCompleteSinglePrice, resultPrice);
+        assertEquals(expectedCompleteSinglePrice, resultPrice);
+    }
+
+    @Test
+    public void double_room_with_checkin_on_thursday_and_checkout_on_sunday_should_return_complete_double_price() {
+        int expectedCompleteDoublePrice = 510;
+        CheckinDate checkinDate = new CheckinDate(LocalDate.of(2019, 10, 17), LocalTime.of(15, 0));
+        LocalDateTime checkoutDate = LocalDateTime.of(2019, 10, 20, 15, 0);
+        String accomodation = "double";
+
+        int resultPrice = calculatePrice(checkinDate, checkoutDate, accomodation);
+
+        assertEquals(expectedCompleteDoublePrice, resultPrice);
     }
 
     private int calculatePrice(CheckinDate checkinDate, LocalDateTime checkoutDate, String accommodation) {
-        return 610;
+        if (accommodation.equals("single")) {
+            return 610;
+        }
+        return 510;
     }
 }
