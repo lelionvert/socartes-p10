7cb04bb44721ce763073b1f651ba39cf18f2c5cb
Yohan F <yohan.fairfort@lacombedulionvert.fr>
2019-08-21 15:07:35 +0200

    Parameterize test and remove setup.

--- promo-10/SocratesTest/RegistrationTest.cs
+++ promo-10/SocratesTest/RegistrationTest.cs
@@ -7,17 +7,12 @@ namespace SocratesTest
 {
     public class RegistrationTest
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void given_a_checkin_on_thursday_and_checkout_on_sunday_lunch_with_single_choice_the_price_should_be_single_complete_price()
+        [TestCase(610)]
+        public void given_a_checkin_on_thursday_and_checkout_on_sunday_lunch_with_a_choice_the_price_should_be_complete_price(
+                int completePrice)
         {
             int totalAmount = Registration.Price();
-            int singleCompletePrice = 610;
-            totalAmount.Should().Be(singleCompletePrice);
+            totalAmount.Should().Be(completePrice);
         }
     }
 }
