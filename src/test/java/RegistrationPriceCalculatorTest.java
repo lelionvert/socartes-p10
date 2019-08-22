d67c50704ac5234bfd748379221cb4afb4dc1865
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-08-22 11:08:00 +0200

    Added acceptance test cases for full stay scenario

--- src/test/java/RegistrationPriceCalculatorTest.java
+++ src/test/java/RegistrationPriceCalculatorTest.java
@@ -8,9 +8,11 @@ import static org.junit.Assert.assertEquals;
 
 public class RegistrationPriceCalculatorTest {
 
+
+    public static final int MEAL_PRICE = 40;
+
     @Test
-    public void acceptance_test_complete_price_scenario() {
-        int mealPrice = 40;
+    public void acceptance_test_single_room_with_full_stay() {
 
         Map<RoomType, Integer> roomTypePrices = initializeRoomTypePrice();
 
@@ -19,7 +21,7 @@ public class RegistrationPriceCalculatorTest {
         Stay conferenceFullStay = new Stay(conferenceStart, conferenceEnd);
 
         RegistrationPriceCalculator registrationPriceCalculator = new RegistrationPriceCalculator(
-                mealPrice, conferenceFullStay, roomTypePrices);
+                MEAL_PRICE, conferenceFullStay, roomTypePrices);
 
         LocalDateTime participantArrival = LocalDateTime.of(2019, 10, 17, 18, 0);
         LocalDateTime participantDeparture = LocalDateTime.of(2019, 10, 20, 16, 0);
@@ -29,6 +31,77 @@ public class RegistrationPriceCalculatorTest {
         int expectedPrice = 610;
 
         int calculatedPrice = registrationPriceCalculator.computePrice(RoomType.SINGLE, participantStay);
+
+        assertEquals(expectedPrice, calculatedPrice);
+    }
+
+    @Test
+    public void acceptance_test_double_room_with_full_stay() {
+
+        Map<RoomType, Integer> roomTypePrices = initializeRoomTypePrice();
+
+        LocalDateTime conferenceStart = LocalDateTime.of(2019, 10, 17, 0, 0);
+        LocalDateTime conferenceEnd = LocalDateTime.of(2019, 10, 20, 14, 0);
+        Stay conferenceFullStay = new Stay(conferenceStart, conferenceEnd);
+
+        RegistrationPriceCalculator registrationPriceCalculator = new RegistrationPriceCalculator(
+                MEAL_PRICE, conferenceFullStay, roomTypePrices);
+
+        LocalDateTime participantArrival = LocalDateTime.of(2019, 10, 17, 18, 0);
+        LocalDateTime participantDeparture = LocalDateTime.of(2019, 10, 20, 16, 0);
+        Stay participantStay = new Stay(participantArrival, participantDeparture);
+
+
+        int expectedPrice = 510;
+
+        int calculatedPrice = registrationPriceCalculator.computePrice(RoomType.DOUBLE, participantStay);
+
+        assertEquals(expectedPrice, calculatedPrice);
+    }
+
+    @Test
+    public void acceptance_test_triple_room_with_full_stay() {
+
+        Map<RoomType, Integer> roomTypePrices = initializeRoomTypePrice();
+
+        LocalDateTime conferenceStart = LocalDateTime.of(2019, 10, 17, 0, 0);
+        LocalDateTime conferenceEnd = LocalDateTime.of(2019, 10, 20, 14, 0);
+        Stay conferenceFullStay = new Stay(conferenceStart, conferenceEnd);
+
+        RegistrationPriceCalculator registrationPriceCalculator = new RegistrationPriceCalculator(
+                MEAL_PRICE, conferenceFullStay, roomTypePrices);
+
+        LocalDateTime participantArrival = LocalDateTime.of(2019, 10, 17, 18, 0);
+        LocalDateTime participantDeparture = LocalDateTime.of(2019, 10, 20, 16, 0);
+        Stay participantStay = new Stay(participantArrival, participantDeparture);
+
+
+        int expectedPrice = 410;
+
+        int calculatedPrice = registrationPriceCalculator.computePrice(RoomType.TRIPLE, participantStay);
+
+        assertEquals(expectedPrice, calculatedPrice);
+    }
+    @Test
+    public void acceptance_test_no_accommodation_with_full_stay() {
+
+        Map<RoomType, Integer> roomTypePrices = initializeRoomTypePrice();
+
+        LocalDateTime conferenceStart = LocalDateTime.of(2019, 10, 17, 0, 0);
+        LocalDateTime conferenceEnd = LocalDateTime.of(2019, 10, 20, 14, 0);
+        Stay conferenceFullStay = new Stay(conferenceStart, conferenceEnd);
+
+        RegistrationPriceCalculator registrationPriceCalculator = new RegistrationPriceCalculator(
+                MEAL_PRICE, conferenceFullStay, roomTypePrices);
+
+        LocalDateTime participantArrival = LocalDateTime.of(2019, 10, 17, 18, 0);
+        LocalDateTime participantDeparture = LocalDateTime.of(2019, 10, 20, 16, 0);
+        Stay participantStay = new Stay(participantArrival, participantDeparture);
+
+
+        int expectedPrice = 240;
+
+        int calculatedPrice = registrationPriceCalculator.computePrice(RoomType.NO_ACCOMMODATION, participantStay);
 
         assertEquals(expectedPrice, calculatedPrice);
     }
