8789989b58555f8a73855cf2869534a727a1ba10
yohan <damien.lequeux@gmail.com>
2019-08-22 11:45:09 +0200

    add unit test for eraly checkout

--- src/test/java/socrates/RegistrationTest.java
+++ src/test/java/socrates/RegistrationTest.java
@@ -8,14 +8,25 @@ import java.time.LocalDateTime;
 public class RegistrationTest {
 
     @Test
-    void number_of_meal_for_complete_stay_should_return_zero() {
+    void number_of_missing_meal_for_complete_stay_should_return_zero() {
         CheckIn checkin = new CheckIn(LocalDateTime.of(2019, 10, 17, 0, 0));
         LocalDateTime checkout = LocalDateTime.of(2019, 10, 20, 15, 00);
 
         Assertions.assertEquals(0, numberOfMissingMeal(checkin, checkout));
     }
 
+    @Test
+    void number_of_missing_meal_for_early_checkout_should_return_1() {
+        CheckIn checkin = new CheckIn(LocalDateTime.of(2019, 10, 17, 0, 0));
+        LocalDateTime checkout = LocalDateTime.of(2019, 10, 19, 18, 0);
+
+        Assertions.assertEquals(1, numberOfMissingMeal(checkin, checkout));
+    }
+
     private int numberOfMissingMeal(CheckIn checkin, LocalDateTime checkout) {
+        if (checkout.isBefore(LocalDateTime.of(2019, 10, 20, 0, 0))) {
+            return 1;
+        }
         return 0;
     }
 }
