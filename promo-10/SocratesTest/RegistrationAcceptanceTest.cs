cd9e2244985855694dddefb3822c4da415ddcba2
Yohan F <yohan.fairfort@lacombedulionvert.fr>
2019-08-22 11:54:37 +0200

    Add second acceptance test of price without one meal Add acceptance test of price without 2 meal

--- promo-10/SocratesTest/RegistrationAcceptanceTest.cs
+++ promo-10/SocratesTest/RegistrationAcceptanceTest.cs
@@ -39,7 +39,7 @@ namespace SocratesTest
 
         [Test]
         public void
-            given_a_checkin_on_thursday_and_checkout_on_saturday_dinner_with_a_single_choice_then_the_price_should_be_610_minus_one_meal()
+            given_a_checkin_on_thursday_and_early_checkout_on_saturday_dinner_with_a_single_choice_then_the_price_should_be_610_minus_one_meal()
         {
             int mealPrice = 40;
             int singleRoomCompletePrice = 610;
@@ -48,7 +48,7 @@ namespace SocratesTest
             DateTime saturdayDinerTime = new DateTime(2019, 10, 21, 18, 0, 0);
             
             CheckIn checkIn = new CheckIn(conferenceStart);
-            CheckOut checkOut = new CheckOut(saturdayDinerTime.AddHours(1));
+            CheckOut earlyCheckOut = new CheckOut(saturdayDinerTime.AddHours(1));
             Dictionary<RoomType, int> roomTypesPrice = new Dictionary<RoomType, int>
             {
                 {RoomType.Single, 610},
@@ -58,9 +58,59 @@ namespace SocratesTest
             };
             
             int totalAmount = new Registration(mealPrice, roomTypesPrice, conferenceStart, conferenceEnd)
-                .Price(RoomType.Single, checkIn, checkOut);
+                .Price(RoomType.Single, checkIn, earlyCheckOut);
             
             totalAmount.Should().Be(singleRoomCompletePrice - mealPrice);
+        }
+
+
+        [Test]
+        public void
+            given_late_checkin_on_friday_and_checkout_on_sunday_with_a_single_choice_then_the_price_should_be_610_minus_one_meal()
+        {
+            int mealPrice = 40;
+            int singleRoomCompletePrice = 610;
+            DateTime conferenceStart = new DateTime(2019, 10, 19);
+            DateTime conferenceEnd = new DateTime(2019, 10, 22);
+            DateTime friday = conferenceStart.AddDays(1);
+            
+            CheckIn lateCheckIn = new CheckIn(friday);
+            CheckOut checkOut = new CheckOut(conferenceEnd);
+            Dictionary<RoomType, int> roomTypesPrice = new Dictionary<RoomType, int>
+            {
+                {RoomType.Single, 610},
+                {RoomType.Double, 510},
+                {RoomType.Triple, 410},
+                {RoomType.NoAccommodation, 240},
+            };
+            
+            int totalAmount = new Registration(mealPrice, roomTypesPrice, conferenceStart, conferenceEnd)
+                .Price(RoomType.Single, lateCheckIn, checkOut);
+            
+            totalAmount.Should().Be(singleRoomCompletePrice - mealPrice);
+        }
+
+        [Test]
+        public void
+            given_late_checkin_on_friday_and_early_checkout_on_saturday_with_a_single_choice_then_the_price_should_be_610_minus_two_meal()
+        {
+            int mealPrice = 40;
+            int singleRoomCompletePrice = 610;
+            DateTime conferenceStart = new DateTime(2019, 10, 19);
+            DateTime conferenceEnd = new DateTime(2019,10,22);
+
+            CheckIn friday = new CheckIn(conferenceStart.AddDays(1));
+            CheckOut saturday = new CheckOut(new DateTime(2019,10,21,18,0,0));
+            Dictionary<RoomType, int> roomTypesPrice  = new Dictionary<RoomType, int>
+            {
+                {RoomType.Single, 610},
+                {RoomType.Double, 510},
+                {RoomType.Triple, 410},
+                {RoomType.NoAccommodation, 240},
+            };
+            int totalAmount = new Registration(mealPrice, roomTypesPrice, conferenceStart, conferenceEnd)
+                .Price(RoomType.Single, friday, saturday);
+            totalAmount.Should().Be(singleRoomCompletePrice - (2 * mealPrice));
         }
     }
 }
